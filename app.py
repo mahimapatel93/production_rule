925f101825b0f9a2c64b1991d2a1551b5489fb55
Mahima Patel <159768314+mahimapatel93@users.noreply.github.com>
2025-12-28 14:27:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Home route
+@app.route("/")
+def home():
+    return "Flask App is Running 🚀"
+
+# Sample GET API
+@app.route("/hello", methods=["GET"])
+def hello():
+    name = request.args.get("name", "Guest")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
+# Sample POST API
+@app.route("/data", methods=["POST"])
+def receive_data():
+    data = request.json
+    return jsonify({
+        "status": "success",
+        "received_data": data
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
